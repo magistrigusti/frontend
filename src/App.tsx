50aba7067a333f019761bf6bc6fee0c6858d1367
Magistry <Magistrigusti@yandex.ru>
2023-07-30 17:05:09 +0300

    my ferst commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import PrivateRoute from './utils/router/privateRoute';
+import Home from './components/home';
+import LoginPage from './components/auth/login';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Home Page</h1>
+
+        <Routes>
+          <Route element={<PrivateRoute />}>
+            <Route element={<Home />} />
+          </Route>
+
+          <Route element={<LoginPage />} />
+        </Routes>
     </div>
   );
 }
